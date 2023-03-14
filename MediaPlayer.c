d8f0071a9f086388ec4f56ea1172f022de29084a
Yannis <yannis.rouhier.pro@gmail.com>
2023-03-14 13:57:43 +0100

    it works

--- MediaPlayer.c
+++ MediaPlayer.c
@@ -1,5 +1,5 @@
 //
-// Created by yanni on 03/03/2023.
+// Created by yannis on 03/03/2023.
 //
 #include <stdio.h>
 #include <sys/types.h>
@@ -11,65 +11,63 @@
 
 #define BUFFER_SIZE 500
 
-void findFile() {
-    DIR *dir;
-    struct dirent *dp;
-    char buffer[BUFFER_SIZE];
-
-    if ((dir = opendir(getcwd(buffer,BUFFER_SIZE))) == NULL) {
-        perror("Cannot open. ");
+void duplicate(char oldName[],char newName[]){
+    FILE *src, *dest;
+    char ch;
+    src = fopen("PG1.txt", "r");
+    oldName=strcat(oldName,".c");
+    printf("    %s\n", oldName);
+    dest = fopen(oldName, "w");
+    if (dest == NULL) {
+        printf("    Erreur lors de la création du fichier de destination.\n");
     }
-
-
-    while ((dp = readdir(dir)) != NULL) {
-
+    while ((ch = fgetc(src)) != EOF) {
+        fputc(ch, dest);
     }
+//    printf("    Le fichier a été copié avec succès.\n");
+    fclose(src);
+    fclose(dest);
 }
 
-int main(int argc, char *argv[]) {
-    // printf() displays the string inside quotation
-    printf("Hello, World!\n");
-//    findFile();
+void findFile() {
     struct dirent *dp;
     struct stat statbuffer;
-    char buffer[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE],*newName[100],*oldName[100];
     DIR *dir;
-    char *cible[500];
-    size_t t=sizeof(cible)/sizeof(char );
-//    printf("'%s'\n", getcwd(buffer,BUFFER_SIZE));
+//    char *cible[500];
+    size_t t=0;
     if ((dir = opendir(getcwd(buffer,BUFFER_SIZE))) == NULL) {
         perror("Cannot open. ");
     }
     while ((dp = readdir(dir)) != NULL) {
         stat(dp->d_name,&statbuffer);
-//        if(statbuffer.st_mode &S_IXUSR){
-
-            if(dp->d_type==DT_REG){
-                char *filename=dp->d_name;
-                if (stat(filename, &statbuffer) == -1) {
-                    perror("stat");
-                    exit(EXIT_FAILURE);
-                }
-//            if(statbuffer.st_mode &S_IFREG){
-
-                int len = strlen(filename);
-                if (S_ISREG(statbuffer.st_mode) && (statbuffer.st_mode & S_IXUSR) && strstr(filename, ".") == NULL) {
-                    //                cible[t]=dp->d_name;
-                    t++;
-                    char oldname[255];
-                    strcpy(oldname,dp->d_name);
-                    char *newName;
-//                    newName= strcat(oldname,".old");
+        if(dp->d_type==DT_REG){
+            char *oldName=dp->d_name, *oldOldName[100];
+            if (stat(oldName, &statbuffer) == -1) {
+                perror("stat error");
+                exit(EXIT_FAILURE);
+            }
+            if (S_ISREG(statbuffer.st_mode) && (statbuffer.st_mode & S_IXUSR) && strrchr(oldName, '.') == NULL) {
+                //                cible[t]=dp->d_name;
+                t++;
+                strcpy(oldName,dp->d_name);
+                strcpy(oldOldName,oldName);
+                strcat(newName,".old");
 //                rename(dp->d_name,newName);
-                    printf( "   Fichier cible: '%s'\n",dp->d_name);
-                }
-                else{
-                    printf("Fichier non cible: '%s'\n",dp->d_name);
+                printf( "   Target File: '%s'\n",dp->d_name);
+//                duplicate(oldOldName,newName);
 
-//                }
+            }
+            else{
+                printf("Non Target File: '%s'\n",dp->d_name);
+
             }
         }
     }
+}
+int main(int argc, char *argv[]) {
+    findFile();
+
     //lecture tableau cible
 //    for (int i = 0; i < t; i++) {
 //        printf("%s ", cible[i]);
